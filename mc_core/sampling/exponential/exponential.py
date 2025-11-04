5833dd7c67dd2d4df7f79c7a0daeac55e68241d1
SaiSampathKedari <sampath@umich.edu>
2025-11-03 20:51:05 -0500

    exponential update

--- mc_core/sampling/exponential/exponential.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from typing import Callable
-
-from mc_core.utils.uniform import uniform_sampleGenerator
-
-
-def exponential_sampleGenerator(lambda_parameter: float, n: int) -> np.ndarray:
-    uniform_samples = np.random.rand(n)
-    return (-1* lambda_parameter*np.log(uniform_samples))
---
+++ mc_core/sampling/uniform.py
@@ -0,0 +1,5 @@
+import numpy as np
+from typing import Callable
+
+def sample_uniform(n: int) -> np.ndarray:
+    return np.random.rand(n)
